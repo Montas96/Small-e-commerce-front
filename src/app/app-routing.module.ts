e7c479721ddfc8bcd9c18995bd90488ab9607ce4
montassar.bouhjila <montassar.bouhjila@gmail.com>
2021-04-25 12:48:52 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/Home', pathMatch: 'full' },
+  { path: 'Home', component: HomePageComponent },
+  { path: 'Home/:categoryId', component: HomePageComponent },
+  { path: 'Product/:id', component: ProductDetailPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { CategoryModule } from './category/category.module';
+import { NavBarModule } from './nav-bar/nav-bar.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProductDetailPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavBarModule,
+    CategoryModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
